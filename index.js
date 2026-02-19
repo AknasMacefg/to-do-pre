d1f7f0a6b55cd3f4dc644e0fa504a904511a8572
AknasMacefg <alex.maslov14@gmail.com>
2026-02-19 16:51:54 +0300

    final update

--- index.js
+++ index.js
@@ -12,24 +12,75 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	const tasks = localStorage.getItem("tasks");
+	if (tasks) {
+		return JSON.parse(tasks);
+	}
+	return items;
 }
 
 function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+  	const textElement = clone.querySelector(".to-do__item-text");
+  	const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+  	const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+  	const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+  	textElement.textContent = item;
+	deleteButton.addEventListener("click", () => {
+		clone.remove();
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	duplicateButton.addEventListener("click", () => {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	editButton.addEventListener("click", () => {
+		textElement.contentEditable = "true";
+		textElement.focus();
+
+	});
+
+	textElement.addEventListener("blur", () => {
+		textElement.contentEditable = "false";
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+  return clone;
 }
 
 function getTasksFromDOM() {
-
+	let itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+	let tasks = [];
+	itemsNamesElements.forEach((item) => {
+		tasks.push(item.textContent);
+	});
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+items = loadTasks();
+items.forEach((item) => {
+	const itemElement = createItem(item);
+	listElement.append(itemElement);
+})
+
+formElement.addEventListener("submit", (evt) => {
+		evt.preventDefault();
+		const item = inputElement.value;
+		listElement.prepend(createItem(item));
+		items = getTasksFromDOM();
+		saveTasks(items);
+		inputElement.value = "";
+	});
